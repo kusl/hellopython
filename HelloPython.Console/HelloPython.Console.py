38ed3579355b6ac72b105f905e8fb4a7d5994e86
kushal <kushaldeveloper@gmail.com>
2016-05-30 16:11:31 -0400

    welcome

--- HelloPython.Console/HelloPython.Console.py
+++ HelloPython.Console/HelloPython.Console.py
@@ -1,3 +1,6 @@
-from datetime import datetime
-
-print(datetime.now())
+print("Please enter your name") 
+user_name = input()
+if user_name == 'kushal':
+    print("hello, kushal")
+else: 
+    print("I see you are new here, " + user_name + ". Welcome!")
