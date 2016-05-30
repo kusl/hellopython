decc1702424c309bf85c491c65cc24382a09ee84
kushal <kushaldeveloper@gmail.com>
2016-05-30 16:13:23 -0400

    add fibonacci numbers
    from https://docs.python.org/3/tutorial/modules.html

--- HelloPython.Console/HelloPython.Console.py
+++ HelloPython.Console/HelloPython.Console.py
@@ -1,6 +1,16 @@
-print("Please enter your name") 
-user_name = input()
-if user_name == 'kushal':
-    print("hello, kushal")
-else: 
-    print("I see you are new here, " + user_name + ". Welcome!")
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    return result
