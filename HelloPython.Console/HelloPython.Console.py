590eea476cad316fed4081721390f7bed446cf34
kushal <kushaldeveloper@gmail.com>
2016-06-01 15:53:43 -0400

    add cpu count to greeting

--- HelloPython.Console/HelloPython.Console.py
+++ HelloPython.Console/HelloPython.Console.py
@@ -1,6 +1,7 @@
 import fibo
 import os
-print("Hello, and welcome to " + os.name)
+import multiprocessing
+print("Hello, and welcome to " + os.name + " on this computer with " + str(multiprocessing.cpu_count()) + " processor cores/threads")
 while True:
     print("Please enter an integer greater than one and the computer will attempt to give you the fibonacci sequence up to that number.")
     user_input = input()
