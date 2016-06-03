8df93029bbba7e9b7cb3bc7e6eb764b823c71654
kushal <kushaldeveloper@gmail.com>
2016-06-03 08:17:30 -0400

    add fibseries

--- HelloPython.Console/HelloPython.Console.py
+++ HelloPython.Console/HelloPython.Console.py
@@ -4,12 +4,12 @@ import multiprocessing
 import requests
 print("Hello, and welcome to " + os.name + " on this computer with " + str(multiprocessing.cpu_count()) + " processor cores/threads")
 while True:
-    print("Please enter an integer greater than one and the computer will attempt to give you the fibonacci sequence up to that number.")
+    print("Please enter an integer greater than one and the computer will attempt to give the sum of all numbers in the fibonacci sequence up to that number.")
     user_input = input()
     try:
         int_user_input = int(user_input)
         if int_user_input > 1:
-            print(fibo.fib2(int_user_input))
+            print(fibo.fibsum(int_user_input))
         else:
             print("Sorry, I can only work with integers greater than one")
     except ValueError:
--- HelloPython.Console/fibo.py
+++ HelloPython.Console/fibo.py
@@ -14,3 +14,9 @@ def fib2(n):   # return Fibonacci series up to n
         result.append(b)
         a, b = b, a+b
     return result
+
+def fibsum(n): 
+    result = 0
+    for x in fib2(n):
+        result += x
+    return result
